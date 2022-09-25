5db55581274167395dac4d4c84c1c71dcfad7492
Sharlotte <60801210+Sharlottes@users.noreply.github.com>
2022-09-26 06:06:14 +0900

    add setting, update readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,37 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Divider, Stack, TextField, Typography } from '@mui/material'
 import { Container } from '@mui/system'
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
+  const [redirectUrl, setRedirectUrl] = useState("https://kakao-nacksee.vercel.app/trolled")
+  const [duration, setDuration] = useState("10분")
+  const [maxMembers, setMaxMembers] = useState(2)
   const [Kakao, setKakao] = useState<typeof window.Kakao>(null)
-  const router = useRouter()
   useEffect(()=>{
     setKakao(window.Kakao);
   }, []);
 
   const share = () => {
-    console.log(router.asPath);
     Kakao.Share.sendDefault({
       objectType: 'feed',
       content: {
-        title: '선착순 선물 게임을 시작합니다! 기회는 단 10분간, 선착순 2명에게!',
+        title: `선착순 선물 게임을 시작합니다! 기회는 단 ${duration}간, 선착순 ${maxMembers}명에게!`,
         imageUrl: `https://kakao-nacksee.vercel.app/images/giftTitle.png`,
         imageWidth: 800, imageHeight: 600,
         link: {
-          mobileWebUrl: `https://kakao-nacksee.vercel.app/trolled`,
-          webUrl: `https://kakao-nacksee.vercel.app/trolled`,
+          mobileWebUrl: redirectUrl,
+          webUrl: redirectUrl,
         },
       },
       buttons: [
         {
           title: '지금 달려가기',
           link: {
-            mobileWebUrl: `https://kakao-nacksee.vercel.app/trolled`,
-            webUrl: `https://kakao-nacksee.vercel.app/trolled`,
+            mobileWebUrl: redirectUrl,
+            webUrl: redirectUrl,
           },
         },
       ],
@@ -38,10 +40,40 @@ const Home: NextPage = () => {
 
   return (
   <Container>
-    <Typography variant="h3" width='100%' sx={{display: 'flex', justifyContent: 'center'}}>Kakaolink Sender</Typography>
-    <Box sx={{marginTop: '30px'}}>
-      <Button id="kakaotalk-sharing-btn" variant="outlined" color="primary" onClick={share}>저지르기!</Button>
-    </Box>
+    <Typography variant="h2" sx={{fontWeight: 'bold'}}>Kakaolink Sender</Typography>
+    <Typography variant="body1" sx={{marginTop: '10px'}}>순수한 마음으로 선물을 좋아하는 사람들에게 가짜 선착순 선물을 보내고, 기쁜 마음으로 달려들 그들의 뒤통수를 시원하게 후려쳐보세요! </Typography> 
+    <Button variant="outlined" color="primary" onClick={share} sx={{marginTop: '30px'}}>저지르기!</Button>
+    
+    <Divider textAlign='left'><Typography sx={{fontWeight: 'bold'}}>세부설정</Typography></Divider>
+
+    <div style={{display: 'flex'}}>
+      <Stack direction="column" spacing="10px" sx={{ width: 'max(300px, 30%)'}}>
+        <TextField label="Redirect Url" onChange={(e => setRedirectUrl(e.target.value))} value={redirectUrl} variant="standard" />
+        <div style={{display: 'flex'}}>
+          <TextField sx={{marginRight: '5px'}} label="Amount" type='number' onChange={(e => setMaxMembers(Number(e.target.value)))} value={maxMembers} variant="standard" />
+          <TextField sx={{marginLeft: '5px'}} label="Duration" onChange={(e => setDuration(`${Number(e.target.value.replaceAll(/\D/g, ""))%60}분`))} value={duration} variant="standard" />
+        </div>
+      </Stack>
+
+      <Box sx={{ width: '250px', borderRadius: '15px', marginLeft: '50px' }}>
+        <img src='/images/giftTitle.png' width='250px' style={{ borderRadius: '15px 15px 0px 0px' }}/>
+        <Typography variant='body2' sx={{marginLeft: '10px', marginRight: '10px', fontSize: 12}}>선착순 선물 게임을 시작합니다! 기회는 단 {duration}간, 선착순 {maxMembers}명에게!</Typography>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+          <Link target="_blank" href={redirectUrl}>
+            <Button color="inherit" sx={{width: '95%', backgroundColor: "#F5F5F5", color: 'black', border: 'none'}} variant='outlined'>
+              <Typography sx={{fontSize: 12}}>지금 달려가기</Typography>
+            </Button>
+          </Link>
+        </div>
+        <div style={{ marginTop: '10px', marginLeft: '10px', marginRight: '10px', display: 'flex', justifyContent: 'space-between'}}>
+          <div style={{display: 'flex'}}>
+            <img src='/images/kagift.png' width='12px' height='12px' style={{border: 'solid 2px lightGray', borderRadius: '15px'}} />
+            <Typography sx={{color: '#999999', fontSize: 11, marginLeft: '5px' }}>선물 바로가기</Typography>
+          </div>
+          <ArrowForwardIosRoundedIcon sx={{color: '#999999', width: '12px', height: '12px'}}/>
+        </div>
+      </Box>
+    </div>
   </Container>
   )
 }
--- pages/trolled.tsx
+++ pages/trolled.tsx
@@ -1,6 +1,7 @@
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from 'next/link';
 
 const Trolled: NextPage = () => {
     return (
@@ -9,6 +10,11 @@ const Trolled: NextPage = () => {
             <title>이런! 낚이셨군요!</title>
         </Head>
         <Typography variant="h3" width='100%' sx={{display: 'flex', justifyContent: 'center'}}>하하 낚시지롱!</Typography>
+        <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+            <Link href='/'>
+                <Button variant="contained" color="primary" sx={{marginTop: '30px'}}>나만 당할 순 없지!</Button>
+            </Link>
+        </div>
     </>
     )
 }
