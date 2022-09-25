b24c362cd1d0be24990e3304c7d84a41631aea7b
Sharlotte <60801210+Sharlottes@users.noreply.github.com>
2022-09-26 07:14:42 +0900

    간간

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
     Kakao.Share.sendDefault({
       objectType: 'feed',
       content: {
-        title: `선착순 선물 게임을 시작합니다! 기회는 단 ${duration}간, 선착순 ${maxMembers}명에게!`,
+        title: `선착순 선물 게임을 시작합니다! 기회는 단 ${duration}, 선착순 ${maxMembers}명에게!`,
         imageUrl: `https://kakao-nacksee.vercel.app/images/giftTitle.png`,
         imageWidth: 800, imageHeight: 600,
         link: {
